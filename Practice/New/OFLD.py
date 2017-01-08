07b1a68c80b7bee6e70be4d83a245332fabf2dc5
M10307431 <chiang404@gmail.com>
2017-01-08 21:54:04 +0800

    0108

---
+++ Practice/New/OFLD.py
@@ -0,0 +1,72 @@
+
+tasknum = 5
+uti = 1.0
+if tasknum == 3 and uti ==0.5:
+    Task1 =[{"Exe":0.038,"Period":0.1,"Deadline":0,'Arrival':-0.1,"Remain":0,'id':1,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.019,"Period":0.4,"Deadline":0,'Arrival':-0.4,"Remain":0,'id':2,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.053,"Period":0.8,"Deadline":0,'Arrival':-0.8,"Remain":0,'id':3,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0}]
+    Task2 =[{"Exe":0.147,"Period":0.4,"Deadline":0,'Arrival':-0.4,"Remain":0,'id':1,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.029,"Period":0.4,"Deadline":0,'Arrival':-0.4,"Remain":0,'id':2,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.045,"Period":0.8,"Deadline":0,'Arrival':-0.8,"Remain":0,'id':3,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0}]
+elif tasknum == 5 and uti ==0.5:
+    Task1 =[{"Exe":0.100,"Period":0.4,"Deadline":0,'Arrival':-0.4,"Remain":0,'id':1,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.017,"Period":0.2,"Deadline":0,'Arrival':-0.2,"Remain":0,'id':2,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.022,"Period":0.4,"Deadline":0,'Arrival':-0.4,"Remain":0,'id':3,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.041,"Period":0.8,"Deadline":0,'Arrival':-0.8,"Remain":0,'id':4,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.040,"Period":0.8,"Deadline":0,'Arrival':-0.8,"Remain":0,'id':5,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0}]
+    Task2 =[{"Exe":0.023,"Period":0.1,"Deadline":0,'Arrival':-0.1,"Remain":0,'id':1,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.017,"Period":0.2,"Deadline":0,'Arrival':-0.2,"Remain":0,'id':2,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.024,"Period":0.4,"Deadline":0,'Arrival':-0.4,"Remain":0,'id':3,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.024,"Period":0.4,"Deadline":0,'Arrival':-0.4,"Remain":0,'id':4,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.044,"Period":0.8,"Deadline":0,'Arrival':-0.8,"Remain":0,'id':5,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0}]
+    
+if tasknum == 3 and uti ==1.0:
+    Task1 =[{"Exe":0.029,"Period":0.1,"Deadline":0,'Arrival':-0.1,"Remain":0,'id':1,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.262,"Period":0.4,"Deadline":0,'Arrival':-0.4,"Remain":0,'id':2,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.040,"Period":0.8,"Deadline":0,'Arrival':-0.8,"Remain":0,'id':3,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0}]
+    Task2 =[{"Exe":0.030,"Period":0.1,"Deadline":0,'Arrival':-0.1,"Remain":0,'id':1,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.043,"Period":0.2,"Deadline":0,'Arrival':-0.2,"Remain":0,'id':2,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.387,"Period":0.8,"Deadline":0,'Arrival':-0.8,"Remain":0,'id':3,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0}]
+elif tasknum == 5 and uti ==1.0:
+    Task1 =[{"Exe":0.023,"Period":0.1,"Deadline":0,'Arrival':-0.1,"Remain":0,'id':1,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.023,"Period":0.1,"Deadline":0,'Arrival':-0.1,"Remain":0,'id':2,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.044,"Period":0.2,"Deadline":0,'Arrival':-0.2,"Remain":0,'id':3,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.140,"Period":0.8,"Deadline":0,'Arrival':-0.8,"Remain":0,'id':4,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.102,"Period":0.8,"Deadline":0,'Arrival':-0.8,"Remain":0,'id':5,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0}]
+    Task2 =[{"Exe":0.136,"Period":0.2,"Deadline":0,'Arrival':-0.2,"Remain":0,'id':1,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.020,"Period":0.2,"Deadline":0,'Arrival':-0.2,"Remain":0,'id':2,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.037,"Period":0.4,"Deadline":0,'Arrival':-0.4,"Remain":0,'id':3,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.025,"Period":0.4,"Deadline":0,'Arrival':-0.4,"Remain":0,'id':4,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0},
+                {"Exe":0.043,"Period":0.8,"Deadline":0,'Arrival':-0.8,"Remain":0,'id':5,"Cnt":0,"OFLD":-999,"OFLDorg":-999,"state":0,"GW":1,"VD":0,"batt":1.0,"uti":0,"time":0}]
+    
+#---------------------------------------
+#OFLD: -1 Local     #state: 1 pre-proc
+#           0 Cloud                 2 exec
+#           * Fog                       3 post-proc
+#---------------------------------------
+p_idle = 1.8
+p_comp = 4.4-1.8
+p_trans = 0.5
+offloadingTrans = 0.025
+fogtrans = 0.005
+proc = 0.005
+speedratio = 5
+
+print 'Offloading Decision'
+for t in Task1:
+    engL = (p_idle+p_comp)*t['Exe']
+    engR = 2*((p_idle+p_trans)*(offloadingTrans-proc) + (p_idle+p_comp)*proc)
+    if(engL > engR):
+        t['OFLDorg'] = -1 if(t['Exe'] > 2*offloadingTrans+t['Exe']/speedratio) else 3
+        t['OFLDorg'] = -1 if(2*fogtrans+t['Exe'] > 2*offloadingTrans+t['Exe']/speedratio) else 3
+
+    print "Task1",t['id'],' ',t['OFLDorg']
+
+for t in Task2:
+    engL = (p_idle+p_comp)*t['Exe']
+    engR = 2*((p_idle+p_trans)*(offloadingTrans-proc) + (p_idle+p_comp)*proc)
+    if(engL > engR):
+        t['OFLDorg'] = -1 if(t['Exe'] > 2*offloadingTrans+t['Exe']/speedratio) else 3
+        t['OFLDorg'] = -1 if(2*fogtrans+t['Exe'] > 2*offloadingTrans+t['Exe']/speedratio) else 3
+
+    print "Task2",t['id'],' ',t['OFLDorg']
--- WBAN_Sim/WBAN_Sim/main.cpp
+++ WBAN_Sim/WBAN_Sim/main.cpp
@@ -47,7 +47,7 @@ int NodeNum = 3;	// # of GW Node
 int TaskNum = 20;	// # of Tasks in each GW
 float total_U = 2.0;	// total Utilization
 float lowest_U = 0.05;	// lowest Utilization
-float m = 0.8;			// migration factor	 1.0 <<---energy------------load--->> 0.0
+float m = 0.5;			// migration factor	 1.0 <<---energy------------load--->> 0.0
 
 int period[] = {100, 200, 400, 800, 1000};
 int HyperPeriod = 4000;
@@ -72,7 +72,7 @@ const float p_trans = 0.5;		// wifi trans	0.3
 const int cloudp_idle = 223;	// server idle power (W)
 const int cloudp_actv = 368;	// server active power
 //--------Time--------------------------------------------------------
-const float _traffic = 1;			// 1/bandwidth(0-1.0)  >> 1, 1/0.75, 1/0.5
+const float _traffic = 1/0.5;			// 1/bandwidth(0-1.0)  >> 1, 1/0.75, 1/0.5
 const int proc = 5;
 const int offloadTransfer = 25;	// global trans time (ms)
 const int fogTransfer = 5;		// local trans
